6b58606fa70f868cd4acd37a49ded3ba344bcd06
KevinBaptiste <kevinbaptiste@hotmail.fr>
2026-05-21 09:50:25 +0200

    update: run-win-cleaning.bat

--- Docker/Stack rproxy et serveur web/Files/run-win-cleaning.bat
+++ Docker/Stack rproxy et serveur web/Files/run-win-cleaning.bat
@@ -1,3 +1,4 @@
 ﻿@echo off
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/KevinBaptiste/windows-washing/refs/heads/main/washing.ps1' -OutFile '%USERPROFILE%\Desktop\wash.ps1'"
-powershell -ExecutionPolicy Bypass -File "%USERPROFILE%\Desktop\wash.ps1"
+set "SCRIPT=%USERPROFILE%\Desktop\win-wash.ps1"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/KevinBaptiste/windows-washing/refs/heads/main/win-cleaning.ps1' -OutFile '%SCRIPT%'"
+powershell -ExecutionPolicy Bypass -File "%SCRIPT%"
