3534bb3fc60847ac0796c84d63313dd3b682fb78
KevinBaptiste <kevinbaptiste@hotmail.fr>
2026-05-21 11:30:14 +0200

    update: run-win-cleaning.bat

--- Docker/SrvWeb/Files/run-win-cleaning.bat
+++ Docker/SrvWeb/Files/run-win-cleaning.bat
@@ -1,4 +1,4 @@
-﻿@REM CE SCRIPT .BAT PERMET LE DL DU PS1 ET SON LANCEMENT AVEC POWERSHELL v8
+﻿@REM CE SCRIPT .BAT PERMET LE DL DU PS1 ET SON LANCEMENT AVEC POWERSHELL v9
 @echo off
 set "SCRIPT=%USERPROFILE%\Desktop\win-cleaning.ps1"
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/KevinBaptiste/windows-washing/refs/heads/main/win-cleaning.ps1' -OutFile '%SCRIPT%'"
